90c2f48f2edf5f573a5a5a6552ccb02ce5092bdc
Mohammad Shafiqul Islam <si_shuvo95@hotmail.com>
2020-07-14 02:04:31 +0600

    new user registration

--- thoughtsnideas/views.py
+++ thoughtsnideas/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [{'author': 'Shuvo', 'title': 'Post 1', 'content': 'Content 1', 'date_posted': '11th July, 2020'},
-         {'author': 'Shuvo', 'title': 'Post 2', 'content': 'Content 2', 'date_posted': '11th July, 2020'}]
-
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'thoughtsnideas/index.html', context)
 
 def about(request):
